899ffff21baf5c4713f1ee39f74a9ab72fe4e800
Kunal Kumar <103351541+Kunal-5402@users.noreply.github.com>
2024-03-11 18:41:29 +0530

    Time: 51 ms (67.26%) | Memory: 25.5 MB (59.16%) - LeetSync

--- 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
     int maxProduct(vector<string>& words) {
-        
         int n = words.size();
         int ans = 0;
 
         vector<bitset<26>> v(n);
 
-        for(int i=0;i<n;i++){
+        for(int i = 0; i < n; i++){
+            for(auto &it: words[i]){
+                v[i].set(it - 'a');
+            }
 
-            for(auto &it: words[i])
-                v[i][it-'a'] = 1;
-
-            for(int j=0;j<i;j++){
-                
-                bool flag = true;
-
-                for(int k=0;k<26;k++){
-                    if(v[i][k] && v[j][k]){
-                        flag = false;
-                        break;
-                    }
+            for(int j = 0; j < i; j++){
+                if((v[i] & v[j]).none()){
+                    ans = max(ans, int(words[i].size() * words[j].size()));
                 }
-
-                if(flag)
-                    ans = max(ans,int(words[i].size() * words[j].size()));
             }
         }
 
