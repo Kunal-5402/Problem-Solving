76966b92de9131b2971c573b20ea35f142742da5
Kunal Kumar <103351541+Kunal-5402@users.noreply.github.com>
2024-03-12 12:30:22 +0530

    Time: 11 ms (31.41%) | Memory: 15.4 MB (5.07%) - LeetSync

--- 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -7,11 +7,6 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
-
-1,3,2,-3,-2,5,5,-5,1
-
-1,-,-,-,-,5,-,-,1
-
  */
 class Solution {
 public:
@@ -45,10 +40,8 @@ public:
             if (ans.count(i) == 0)
                 res.push_back(v[i]);
         }
-        if (res.size() == 0)
-            return nullptr;
 
-        if (res[0] == 0)
+        if (res.size() == 0)
             return nullptr;
 
         ListNode* node = new ListNode();
@@ -60,9 +53,6 @@ public:
                 node->next = new ListNode();
                 node = node->next;
             }
-        }
-        for (auto it : res) {
-            cout << it << " ";
         }
         return head;
     }
