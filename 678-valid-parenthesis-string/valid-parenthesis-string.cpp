10dc5745df33e6ba26d6e7b8e725bf6b095c0a41
Kunal Kumar <103351541+Kunal-5402@users.noreply.github.com>
2024-04-08 13:37:13 +0530

    Time: 3 ms (45.67%) | Memory: 7.3 MB (75.53%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,40 +1,28 @@
 class Solution {
-public: 
+public:
     bool checkValidString(string s) {
-        vector<vector<int>> memo(s.size(), vector<int>(s.size(), -1));
-        return isValidString(0, 0, s, memo);
-    }
-private: 
-    bool isValidString(int index, int openCount,
-        const string & str, vector < vector < int >> & memo) {
-        // If reached end of the string, check if all brackets are balanced
-        if (index == str.size()) {
-            return (openCount == 0);
+        int n = s.size();
+        int open = 0;
+        int close = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(' or s[i] == '*')
+                open++;
+            else
+                open--;
+            if (open < 0)
+                return false;
         }
 
-        // If already computed, return memoized result
-        if (memo[index][openCount] != -1) {
-            return memo[index][openCount];
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == ')' or s[i] == '*')
+                close++;
+            else
+                close--;
+            if (close < 0)
+                return false;
         }
-
-        bool isValid = false;
-        // If encountering '*', try all possibilities
-        if (str[index] == '*') {
-            isValid |= isValidString(index + 1, openCount + 1, str, memo); // Treat '*' as '('
-            if (openCount) {
-                isValid |= isValidString(index + 1, openCount - 1, str, memo); // Treat '*' as ')'
-            }
-            isValid |= isValidString(index + 1, openCount, str, memo); // Treat '*' as empty
-        } else {
-            // Handle '(' and ')'
-            if (str[index] == '(') {
-                isValid = isValidString(index + 1, openCount + 1, str, memo); // Increment count for '('
-            } else if (openCount) {
-                isValid = isValidString(index + 1, openCount - 1, str, memo); // Decrement count for ')'
-            }
-        }
-
-        // Memoize and return the result
-        return memo[index][openCount] = isValid;
+        
+        return true;
     }
 };
